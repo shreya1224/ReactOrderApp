26caad8786e1a4602a8f354c628a42b4445da7ae
Piyasha1108 <piyasha.ghosh1108@gmail.com>
2022-06-23 16:35:45 +0530

    Customer and Product Added

---
+++ src/components/ProductList.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ProductService from "../services/ProductService";
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  const init = () => {
+    ProductService.getAll()
+      .then((response) => {
+        console.log("Printing Products", response.data);
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log("Something went wrong", error);
+      });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleDelete = (id) => {
+    console.log("Printing id", id);
+    ProductService.remove(id)
+      .then((response) => {
+        console.log("Product has been Deleted", response.data);
+        init();
+      })
+      .catch((error) => {
+        console.log("Something went wrong", error);
+      });
+  };
+
+  return (
+    <div className="container">
+      <h3>List of Products</h3>
+      <hr />
+      <div>
+        <Link to="/addprod" className="btn btn-primary mb-2">
+          Add Product
+        </Link>
+        <table className="table table-bordered table-striped">
+          <thead className="thead-dark">
+            <tr>
+              <th> Product name</th>
+              <th> Price</th>
+              <th> Actions </th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.pname}</td>
+                <td>{product.price}</td>
+
+                <td>
+                  <Link
+                    className="btn btn-info"
+                    to={`/products/edit/${product.id}`}
+                  >
+                    Update Product
+                  </Link>
+
+                  <button
+                    className="btn btn-danger ml-2"
+                    onClick={() => {
+                      handleDelete(product.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddCustomer from "./components/AddCustomer";
+import CustomerList from "./components/CustomerList";
+import NotFound from "./components/NotFound";
+import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
 import {
   Navigation,
   Footer,
@@ -16,7 +21,6 @@ import WomenStore from "./components/WomenStore";
 import MenStore from "./components/MenStore";
 import Accessories from "./components/Accessories";
 
-
 ReactDOM.render(
   <Router>
     <Navigation />
@@ -24,16 +28,29 @@ ReactDOM.render(
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
- 
+
       <Route path="/store/women" element={<WomenStore />} />
       <Route path="/store/men" element={<MenStore />} />
       <Route path="/store/accessories" element={<Accessories />} />
+      {/* <Route path="/customers" element={<Customer />}> */}
+      <Route exact path="/custlist" element={<CustomerList />} />
+      <Route path="/addcust" element={<AddCustomer />} />
+      <Route path="/customers/edit/:id" element={<AddCustomer />} />
+      <Route path="/*" element={<NotFound />} />
+      {/* </Route> */}
+
+      {/* <Route path="/products" element={<Product />}> */}
+      <Route exact path="/prodlist" element={<ProductList />} />
+      <Route path="/addprod" element={<AddProduct />} />
+      <Route path="/products/edit/:id" element={<AddProduct />} />
+      <Route path="/*" element={<NotFound />} />
+      {/* </Route> */}
+
       {/* <Route path="/get/products" element={<FetchProduct/>}/>
       <Route path="/save/product" element={<SaveProduct/>}/> */}
       <Route path="/blog" element={<Blog />}>
         <Route path="" element={<Posts />} />
         <Route path=":postSlug" element={<Post />} />
-        
       </Route>
     </Routes>
 
