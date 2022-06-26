b2a4fead8dcaa6e122e25adbabee56392bb6d1d6
Piyasha1108 <piyasha.ghosh1108@gmail.com>
2022-06-26 21:52:19 +0530

    css modified

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -11,13 +11,13 @@ export default function UserList() {
         <br></br>
         <br></br>
         <Link to="/user">
-          <button className="button1">Register Yourself</button>
+          <button className="butt1">Register Yourself</button>
         </Link>
         <br></br>
         <br></br>
         <br></br>
         <Link to="/seeorder">
-          <button className="button2">See Your Order</button>
+          <button className="butt2">See Your Order</button>
         </Link>
       </div>
     </div>
